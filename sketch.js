fe4c06a36fa40555c0aed431a3816ac8cde62584
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-03 23:35:31 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 /*
 ----- Coding Tutorial by Patt Vira ----- 
 Name: Interactive Fridge Magnets
@@ -19,7 +18,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(640, 480); // 將畫布大小改為
+  createCanvas(640, 480);
   // Detect video & load ML model
   video = createCapture(VIDEO, {flipped: true});
   video.hide();
@@ -34,16 +33,11 @@ function setup() {
 
 function draw() {
   background(220);
-  
-  // Display video and detect index and thumb position
+
+  // 先顯示 video
   image(video, 0, 0, width, height);
-  
-  // 顯示淡江教育科技學系文字
-  fill(0); // 設定文字顏色為黑色
-  textSize(32); // 設定文字大小
-  textAlign(CENTER, TOP); // 文字置中，從頂部開始
-  text("淡江教育科技學系", width / 2, 10); // 顯示文字於畫布頂部中央
-  
+
+  // 其餘偵測與磁鐵程式碼
   if (hands.length > 0) {
     let index = hands[0].keypoints[8];
     let thumb = hands[0].keypoints[4];
@@ -61,6 +55,13 @@ function draw() {
   for (let i=0; i<num; i++) {
     magnets[i].display();
   }
+
+  // 最後顯示文字在畫面正中央
+  fill(0);
+  textFont(font);
+  textSize(36);
+  textAlign(CENTER, CENTER); // 垂直、水平都置中
+  text("淡江教育科技系", width / 2, height / 2);
 }
 
 function gotHands(results) {
