46d0adb75ff5033a470c81eeb991ae7b2b563a59
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-03 16:01:30 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(800, 600); // 將畫布大小調整為 1200x800
+  createCanvas(800, 600); // 將畫布大小改為 800x600
   // Detect video & load ML model
   video = createCapture(VIDEO, {flipped: true});
   video.hide();
@@ -36,6 +36,13 @@ function draw() {
   
   // Display video and detect index and thumb position
   image(video, 0, 0, width, height);
+  
+  // 顯示淡江教育科技學系文字
+  fill(0); // 設定文字顏色為黑色
+  textSize(32); // 設定文字大小
+  textAlign(CENTER, TOP); // 文字置中，從頂部開始
+  text("淡江教育科技學系", width / 2, 10); // 顯示文字於畫布頂部中央
+  
   if (hands.length > 0) {
     let index = hands[0].keypoints[8];
     let thumb = hands[0].keypoints[4];
@@ -58,4 +65,3 @@ function draw() {
 function gotHands(results) {
   hands = results;
 }
-
