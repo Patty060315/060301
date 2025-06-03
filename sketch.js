e1a17c0abebbe9bfa9a388843fdb088c2c554855
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-04 02:11:30 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(800, 600); // 改為 800*600
+  createCanvas(800, 600);
   // Detect video & load ML model
   video = createCapture(VIDEO, {flipped: true});
   video.hide();
@@ -33,33 +33,24 @@ function setup() {
 
 function draw() {
   background(220);
-
-  // 先顯示攝影機畫面
-  image(video, 0, 0, width, height);
-
-  // 畫面上方中間顯示中文字標題
-  fill(0);
-  textFont(font);
-  textSize(32);
-  textAlign(CENTER, TOP);
-  text("淡江教育科技系", width / 2, 20);
-
+  
   // Display video and detect index and thumb position
+  image(video, 0, 0, width, height);
   if (hands.length > 0) {
     let index = hands[0].keypoints[8];
     let thumb = hands[0].keypoints[4];
-
+    
     noFill();
     stroke(0, 255, 0);
     text("index", index.x, index.y);
     text("thumb", thumb.x, thumb.y);
-
-    for (let i = 0; i < num; i++) {
+  
+    for (let i=0; i<num; i++) {
       magnets[i].touch(thumb.x, thumb.y, index.x, index.y);
     }
   }
-
-  for (let i = 0; i < num; i++) {
+  
+  for (let i=0; i<num; i++) {
     magnets[i].display();
   }
 }
